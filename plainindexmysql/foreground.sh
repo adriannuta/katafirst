78e64df430c63c0c063b729d8356b8bbeded5aa2
Adrian Nuta <adriannuta@gmail.com>
2018-11-28 12:08:01 +0100

    Updated

--- plainindexmysql/foreground.sh
+++ plainindexmysql/foreground.sh
@@ -2,6 +2,7 @@ docker run -d --name mysql  -e MYSQL_ROOT_PASSWORD=simple -e MYSQL_DATABASE=saki
 wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 wget http://downloads.mysql.com/docs/sakila-db.tar.gz
 tar zxvf sakila-db.tar.gz
+chmod +x wait-for-it.sh
 ./wait-for-it.sh -h docker -p 3306 -t 30 --
 cd sakila-db
 mysql -P3306 -h docker -uuser -ppass123 sakila < sakila-schema.sql
