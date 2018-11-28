5777abf95e9b22e69d94fd0703700734e41cf82a
Adrian Nuta <adriannuta@gmail.com>
2018-11-28 11:49:40 +0100

    Updated

--- plainindexmysql/foreground.sh
+++ plainindexmysql/foreground.sh
@@ -1,4 +1,4 @@
-docker run -d --name mysql  -e MYSQL_ROOT_PASSWORD=simple -e MYSQL_DATABASE=sakila -p 3306:3306  -e MYSQL_USER=user -e MYSQL_PASSWORD=pass123  mysql
+docker run  --name mysql  -e MYSQL_ROOT_PASSWORD=simple -e MYSQL_DATABASE=sakila -p 3306:3306  -e MYSQL_USER=user -e MYSQL_PASSWORD=pass123  mysql
 wget http://downloads.mysql.com/docs/sakila-db.tar.gz
 tar zxvf sakila-db.tar.gz
 cd sakila-db
