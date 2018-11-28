4886139992a9631cf0a573e4a71c35ac974d387c
Adrian Nuta <adriannuta@gmail.com>
2018-11-28 11:59:59 +0100

    Updated

--- plainindexmysql/foreground.sh
+++ plainindexmysql/foreground.sh
@@ -1,5 +1,5 @@
-wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 docker run  --name mysql  -e MYSQL_ROOT_PASSWORD=simple -e MYSQL_DATABASE=sakila -p 3306:3306  -e MYSQL_USER=user -e MYSQL_PASSWORD=pass123  mysql
+wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 wget http://downloads.mysql.com/docs/sakila-db.tar.gz
 tar zxvf sakila-db.tar.gz
 cd sakila-db
