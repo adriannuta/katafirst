a11c8de9964f72acd6cc36c8f870581f89bb05ac
Adrian Nuta <adriannuta@gmail.com>
2018-12-09 21:07:34 +0100

    Updated

--- plainindexmysql/foreground.sh
+++ plainindexmysql/foreground.sh
@@ -1,5 +1,2 @@
 docker run -d --name sakilaexample  manticoresearch/sakilaexample
-wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-chmod +x wait-for-it.sh
-./wait-for-it.sh -h docker -p 3306 -t 30 --
 docker exec -it sakilaexample bash
