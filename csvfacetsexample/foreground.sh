eaebde6b2161faeefea8bbf288939aced5193de0
Adrian Nuta <adriannuta@gmail.com>
2019-02-07 21:04:55 +0100

    Updated

--- csvfacetsexample/foreground.sh
+++ csvfacetsexample/foreground.sh
@@ -1,4 +1,4 @@
-docker run -d --name geoexample  manticoresearch/csvexample
+docker run -d --name csvexample  manticoresearch/csvexample
 docker exec -it csvexample sh
 cd /test
 tar zxvf movie_metadata.tar.gz  
